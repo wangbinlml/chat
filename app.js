3954355bcd62117f1b9495f4b87acbc5ab1f1acb
wangbin <wangbin@3ikids.com>
2015-02-11 10:27:19 +0800

    passport 登录认证

--- app.js
+++ app.js
@@ -4,10 +4,13 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var flash = require('connect-flash');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
-
 var app = express();
 
 // view engine setup
@@ -18,15 +21,33 @@ app.set('view engine', 'ejs');
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
+app.use(require('express-session')({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(flash());
 
 app.use('/', routes);
 app.use('/users', users);
 
+
+// passport config
+var Account = require('./models/account');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
+
+// mongoose
+mongoose.connect('mongodb://localhost/passport_local');
+
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
@@ -37,7 +58,7 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
+    app.use(function (err, req, res, next) {
         res.status(err.status || 500);
         res.render('error', {
             message: err.message,
@@ -48,13 +69,18 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
         message: err.message,
         error: {}
     });
 });
+app.all('/users', isLoggedIn);
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated())
+        return next();
 
-
+    res.redirect('/');
+}
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,62 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
+var Account = require('../models/account');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res) {
+    res.render('index', {
+        user: req.user,
+        title: 'Express',
+        success: req.flash('success').toString(),
+        error: req.flash('error').toString()
+    });
+});
+
+router.get('/register', function (req, res) {
+    res.render('register', {
+        user: req.user,
+        title: 'Express',
+        success: req.flash('success').toString(),
+        error: req.flash('error').toString()
+    });
+});
+
+router.post('/register', function (req, res) {
+    Account.register(new Account({username: req.body.username}), req.body.password, function (err, account) {
+        if (err) {
+            return res.render('register', {
+                user: req.user,
+                title: 'Express',
+                account: account
+            });
+        }
+
+        passport.authenticate('local')(req, res, function () {
+            res.redirect('/');
+        });
+    });
+});
+router.get('/login', function (req, res) {
+    res.render('index', {
+        user: req.user,
+        title: 'Express',
+        success: req.flash('success').toString(),
+        error: req.flash('error').toString()
+    });
+});
+router.post('/login', passport.authenticate('local', {
+    successRedirect: '/users',
+    failureRedirect: '/'
+}));
+
+router.get('/logout', function (req, res) {
+    req.logout();
+    res.redirect('/');
+});
+
+router.get('/ping', function (req, res) {
+    res.status(200).send("pong!");
 });
 
 module.exports = router;
